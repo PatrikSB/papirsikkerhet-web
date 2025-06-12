34af1bdcb07df6b365a11a1ed4ce1feab07c4101
Patrik Bakke <patrik@seldalbakke.no>
2025-06-12 11:47:51 +0200

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <h2 className="mb-3 text-2xl font-semibold">
-            NEI!
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Har jeg gjort alt av compliance?
-          </p>
-      </div>
-    </main>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="tracking-[-.01em]">
+            JA, Papirsikkerhet er også viktig!
+          </li>
+        </ol>
+      </main>
+    </div>
   );
 }
